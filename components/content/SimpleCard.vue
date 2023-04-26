0364ed0872fbdcd5575e5c7d503da02daf6a3eb4
crearis <hans.doenitz@dasei.eu>
2023-04-26 13:14:33 +0200

    added bootstrap-card

---
+++ components/content/SimpleCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { CCard, CCardHeader, CCardTitle, CCardBody } from '@coreui/vue'
+  import '@coreui/coreui/dist/css/coreui.min.css'
+</script>
+
+<template>
+  <CCard text-color="primary" class="mb-3 border-top-3" :class="'border-top-' + 'primary'" style="max-width: 18rem">
+    <CCardHeader>Header</CCardHeader>
+    <CCardBody>
+      <CCardTitle text-color="primary" color="primary">card title</CCardTitle>
+      <ContentSlot :use="$slots.default" unwrap="p" />
+    </CCardBody>
+  </CCard>
+</template>
